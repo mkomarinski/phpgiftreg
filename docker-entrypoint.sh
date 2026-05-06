96a5cfb264af526c48171cd63221e3493aabd623
Mark Komarinski <mkomarinski@wayga.org>
2026-05-05 20:54:09 -0400

    make directories before chowning

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# Create required directories if they don't exist (needed due to volume mounts)
+mkdir -p /var/www/html/item_images
+mkdir -p /var/www/html/templates_c
+mkdir -p /var/www/html/cache
+
 # Fix ownership of mounted volumes for www-data
 chown -R www-data:www-data /var/www/html
 chmod -R 755 /var/www/html
