5c08f46a687908791c0ec6fe695ed9e016f6f579
Mark Komarinski <mkomarinski@wayga.org>
2026-05-07 15:09:46 -0400

    don't use external environment files

--- src/includes/config.php
+++ src/includes/config.php
@@ -13,37 +13,6 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-function loadEnvFile($path) {
-	if (!file_exists($path) || !is_readable($path)) {
-		return;
-	}
-
-	$lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-	foreach ($lines as $line) {
-		$line = trim($line);
-		if ($line === '' || $line[0] === '#') {
-			continue;
-		}
-		if (strpos($line, '=') === false) {
-			continue;
-		}
-
-		list($name, $value) = explode('=', $line, 2);
-		$name = trim($name);
-		$value = trim($value);
-		$value = trim($value, " \t\n\r\0\x0B\"");
-		if ($name === '') {
-			continue;
-		}
-
-		if (getenv($name) === false) {
-			putenv("$name=$value");
-		}
-		$_ENV[$name] = $value;
-		$_SERVER[$name] = $value;
-	}
-}
-
 if (!function_exists('getEnvOrDefault')) {
 	function getEnvOrDefault($name, $default = null) {
 		$value = getenv($name);
@@ -104,8 +73,6 @@ function getDefaultConfigOptions() {
 	);
 }
 
-loadEnvFile(dirname(__FILE__) . "/../.env");
-
 function getGlobalOptions($refresh = false) {
 	static $opt;
 	if (!isset($opt) || $refresh) {
@@ -125,122 +92,3 @@ function getGlobalOptions($refresh = false) {
 	}
 	return $opt;
 }
-
-	$db_host = getEnvOrDefault('DB_HOST', 'localhost');
-	$db_name = getEnvOrDefault('DB_NAME', 'giftreg');
-	$db_user = getEnvOrDefault('DB_USER', 'giftreg');
-	$db_password = getEnvOrDefault('DB_PASSWORD', 'cn3Malk');
-	$db_port = getEnvOrDefault('DB_PORT', '3306');
-	
-	return array(
-		/* The PDO connection string.
-			http://www.php.net/manual/en/pdo.connections.php
-		*/
-		"pdo_connection_string" => "mysql:host={$db_host};port={$db_port};dbname={$db_name}",
-
-		/* The database username and password. */
-		"pdo_username" => $db_user,
-		"pdo_password" => $db_password,
-
-		/* The maximum number of days before an event which produces a notification. */
-		"event_threshold" => "60",
-
-		/* Whether or not requesting to shop for someone is immediately approved. 
-			0 = auto-approve,
-			1 = require approval
-		*/
-		"shop_requires_approval" => 1,
-
-		/* Whether or not requesting a new account is immediately approved.
-			0 = auto-approve,
-			1 = require administrator approval
-		*/
-		"newuser_requires_approval" => 1,
-
-		/* Whether or not whom an item is reserved/bought by is hidden. */
-		"anonymous_purchasing" => 0,
-
-		/* The number of your items that show on each page. */
-		"items_per_page" => 10,
-
-		/* The e-mail From: header. */
-		"email_from" => "webmaster@" . $_SERVER['SERVER_NAME'],
-
-		/* The e-mail Reply-To: header. */
-		"email_reply_to" => "mkomarinski@wayga.org",
-
-		/* The e-mail X-Mailer header. */
-		"email_xmailer" => "PHP/" . phpversion(),
-
-		/* Whether or not to show brief blurbs in certain spots which describe how 
-			features work.
-			0 = don't help text,
-			1 = show help text
-		*/
-		"show_helptext" => 0,
-
-		/* Whether or not clicking the Delete Item link requires a JavaScript-based
-			confirmation.
-			0 = don't show confirmation,
-			1 = show confirmation
-		*/
-		"confirm_item_deletes" => 0,
-
-		/* Whether or not to allow multiple quantities of an item. */
-		"allow_multiples" => 1,
-
-		/* This is prefixed to all currency values, set it as appropriate for your currency. */
-		"currency_symbol" => "$",	// US or other dollars      
-		//"currency_symbol" => "&#163;",	// Pound (�) symbol
-		//"currency_symbol" => "&#165;",	// Yen
-		//"currency_symbol" => "&#8364;",	// Euro
-		//"currency_symbol" => "&euro;",	// Euro alternative
-
-		/* The date format used in DateTime::format()
-			http://php.net/manual/en/function.date.php */
-		"date_format" => "m/d/Y",
-
-		/* If this is set to something other than "" then phpgiftreg will expect that
-			string to prefix all tables in this installation.  Useful for running
-			multiple phpgiftreg installations in the same MySQL database.
-		*/
-		"table_prefix" => "",
-		//"table_prefix" => "gift_",		// all tables must be prefixed by `gift_'
-
-		/* Whether or not your own events show up on the home page.
-			0 = don't show my own events,
-			1 = show my own events
-		*/
-		"show_own_events" => 1,
-
-		/* The length of random generated passwords. */
-		"password_length" => 8,
-
-		/* Whether or not to hide the price when it's $0.00.
-			0 = don't hide it,
-			1 = hide it
-		*/
-		"hide_zero_price" => 1,
-
-		/* Whether or not to allow image uploads.  If on, the next option must point to
-			a valid subdirectory that is writeable by the web server.  The setup.php
-			script will confirm this.
-			0 = don't allow images,
-			1 = allow images
-		*/
-		"allow_images" => 1,
-
-		/* The *sub*-directory we we can store item images.  If you don't want to
-			allow images to be attached to items, leave this variable empty ("").
-			Trailing / is optional.
-		*/
-		"image_subdir" => "item_images",
-		
-		/* The number of minutes in between subscription notifications so the subscribers
-			don't get flooded with updates.
-		*/
-		"notify_threshold_minutes" => 60
-	);
-}
-
-
