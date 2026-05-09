2ccf2706c39cd6efa152a1837501cc399194f4f2
Mark Komarinski <mkomarinski@wayga.org>
2026-05-08 21:44:56 -0400

    clean up sql files

--- src/sql/create-phpgiftregdb.sql
+++ src/sql/create-phpgiftregdb.sql
@@ -171,3 +171,12 @@ CREATE TABLE `subscriptions` (
 	`last_notified` datetime DEFAULT NULL,
 	PRIMARY KEY (`publisher`,`subscriber`)
 );
+
+CREATE TABLE `guardianships` (
+  `guardian_userid` int(11) NOT NULL,
+  `child_userid` int(11) NOT NULL,
+  `relationship_type` varchar(50) NOT NULL default 'parent',
+  PRIMARY KEY (`guardian_userid`,`child_userid`),
+  FOREIGN KEY (`guardian_userid`) REFERENCES `users`(`userid`) ON DELETE CASCADE,
+  FOREIGN KEY (`child_userid`) REFERENCES `users`(`userid`) ON DELETE CASCADE
+);
